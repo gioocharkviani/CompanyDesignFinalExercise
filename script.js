14cc55e195b38918cb43cd8d96df36b14dea93ff
gioocharkviani <gioocharkviani@gmail.com>
2023-01-23 16:30:38 +0400

    gggg

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const SliderSelector = [...document.querySelectorAll('.SliderWrapper1')]
+const SliderBtn = [...document.querySelectorAll('.SliderBtn')]
+const SliderBtnde = [...document.querySelectorAll('.SliderBtnactive')]
 
 SliderSelector.forEach((Slide , i) => {
 let SliderBoxDimision = Slide.getBoundingClientRect();
 let SliderBoxWidth = SliderBoxDimision.width;
-console.log(SliderBoxWidth);
+
+SliderBtn[i++].addEventListener('click' , () => {
+    Slide.scrollLeft += SliderBoxWidth;
+})
+
+SliderBtnde[i].addEventListener('click' , () => {
+    Slide.scrollLeft -= SliderBoxWidth;
+})
+
 
 
 })
